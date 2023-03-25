c0d06b99c56b562b6312f17e1bd2fe7a743ebeba
Ignacio <126260124+P1P0023@users.noreply.github.com>
2023-03-25 15:56:05 -0300

    initial commit

---
+++ boton.js
@@ -0,0 +1,5 @@
+
+document.getElementById('boton').onclick = funtion () {
+    console.log("Capturamos el evento click");
+    document.getElementById("demo").innerHTML = "en el curzo de diseño grafico estuvimos viendo cosas pcomo glitch, neon, o incluso como poner lo aprendido en produtos como tazas o remeras";
+}
